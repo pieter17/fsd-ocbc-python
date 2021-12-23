449a047c92b05ebdf2bff77e1cbd44e71895eb38
Pieter Wenji <pieterjake321@gmail.com>
2021-12-23 12:55:46 +0700

    add sesi 9

---
+++ sesi9/app.py
@@ -0,0 +1,33 @@
+"""
+Main module of the server file
+"""
+
+# 3rd party modules
+from flask import render_template
+
+# local modules
+import config
+
+# get the application instance
+connex_app = config.connex_app
+
+# Read the swagger.yml file to configure the endpoints
+connex_app.add_api("swagger.yml")
+
+# for flask not found error
+connex_app = connex_app.app
+
+
+# create a URL route in our application for "/"
+@connex_app.route('/')
+def home():
+    """
+    This function just responds to the browser URL
+    localhost:5000/
+    :return:        the rendered template "home.html"
+    """
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    connex_app.run(debug=True)
