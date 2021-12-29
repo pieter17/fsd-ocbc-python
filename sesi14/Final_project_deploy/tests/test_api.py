1e1122c26e5df210f52a735a5127d37b97572cd7
Pieter Wenji <pieterjake321@gmail.com>
2021-12-29 11:46:09 +0700

    add sesi 14 and doc

--- sesi14/Final_project_deploy/tests/test_api.py
+++ sesi14/Final_project_deploy/tests/test_api.py
@@ -9,8 +9,14 @@ connex_app = connex_app.app
 
 client = connex_app.test_client()
 
+mock_request_headers = {'Content-Type': 'application/json'}
+
 
 def test_director_read_all():
+    """
+    test GET director
+    """
+
     url = '/api/director?limit=3'
 
     response = client.get(url)
@@ -20,6 +26,9 @@ def test_director_read_all():
 
 
 def test_director_read_all_limit():
+    """
+    test get director with limit
+    """
     limit = 253
     url = f'/api/director?limit={limit}'
 
@@ -31,6 +40,9 @@ def test_director_read_all_limit():
 
 
 def test_director_read_one():
+    """
+    test get director with id
+    """
     director_id = 4762
     url = f'/api/director/{director_id}'
 
@@ -43,6 +55,9 @@ def test_director_read_one():
 
 
 def test_director_read_one_404():
+    """
+    test get with wrong value
+    """
     director_id = 1
     url = f'/api/director/{director_id}'
 
@@ -55,9 +70,11 @@ def test_director_read_one_404():
 
 
 def test_post_director_500():
+    """
+    test post with wrong value
+    """
     url = '/api/director'
     mock_data_fail = {"department": ""}
-    mock_request_headers = {'Content-Type': 'application/json'}
 
     res = client.post(url,
                       data=json.dumps(mock_data_fail),
@@ -72,10 +89,11 @@ mock_data_director = {
     "uid": random.randint(11111, 999999)
 }
 
-mock_request_headers = {'Content-Type': 'application/json'}
-
 
 def test_post_director_create():
+    """
+    test post director
+    """
     url = '/api/director'
 
     res = client.post(url,
@@ -88,6 +106,9 @@ def test_post_director_create():
 
 
 def test_put_director():
+    """
+    test put director
+    """
     director = (Directors.query.filter(
         Directors.name == mock_data_director['name'])
     ).outerjoin(Movies).one_or_none()
@@ -107,6 +128,9 @@ def test_put_director():
 
 
 def test_delete_director():
+    """
+    test delete director
+    """
     director = (Directors.query.filter(
         Directors.name == mock_data_director['name'])
     ).outerjoin(Movies).one_or_none()
@@ -119,7 +143,11 @@ def test_delete_director():
 
 ###################################################################################################################
 
+
 def test_movie_read_all():
+    """
+    test get movie
+    """
     url = '/api/movie?limit=3'
 
     response = client.get(url)
@@ -130,6 +158,9 @@ def test_movie_read_all():
 
 
 def test_movie_read_all_limit():
+    """
+    test get movie with limit
+    """
     limit = 322
     url = f'/api/movie?limit={322}'
 
@@ -141,6 +172,9 @@ def test_movie_read_all_limit():
 
 
 def test_movie_read_one():
+    """
+    test get movie with id
+    """
     director_id = 4762
     movie_id = 43597
     url = f'/api/director/{director_id}/movie/{movie_id}'
@@ -155,6 +189,9 @@ def test_movie_read_one():
 
 
 def test_movie_read_one_404():
+    """
+    test get movie with wrong id
+    """
     director_id = 1
     movie_id = 123
     url = f'/api/director/{director_id}/movie/{movie_id}'
@@ -183,6 +220,9 @@ mock_data_movie = {
 
 
 def test_post_movie_create():
+    """
+    test post movie
+    """
     director_id = 4768
     url = f'/api/movie/{director_id}/movie'
     res = client.post(url,
@@ -195,6 +235,9 @@ def test_post_movie_create():
 
 
 def test_put_movie():
+    """
+    test put movie
+    """
     director_id = 4768
     movie = (Movies.query.filter(
         Movies.title == mock_data_movie['title']).filter(
@@ -215,6 +258,9 @@ def test_put_movie():
 
 
 def test_delete_movie():
+    """
+    test delete movie
+    """
     director_id = 4768
     movie = (Movies.query.filter(
         Movies.title == mock_data_movie['title']).filter(
